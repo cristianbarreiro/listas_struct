bd90e910c10ae730c7076f46df6927e28ebf59d8
C. Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-10-08 21:27:03 -0300

    fix: .h

--- arboles_fin_phsh/arbol.h
+++ arboles_fin_phsh/arbol.h
@@ -7,5 +7,14 @@
 
 typedef struct nodo_arbol *arbol; // tipo opaco
 
+arbol crear();
+arbol primer_hijo(arbol a);
+arbol siguiente_hermano(arbol a);
+int cantidad(arbol a);
+bool pertenece(int x, arbol a);
+int profundidad(arbol a);   
+bool vacio(arbol a);
+arbol destruir(arbol a);
+arbol insertar(int x, int padre, arbol a);
 
 #endif
