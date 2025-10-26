46c3e1576e9a87e9a9f1bbe09d9817daa87eab37
C. Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-10-26 19:50:46 -0300

    feat: estructura proyecto parte 1/2

--- estructuras_proyecto/estructura.cpp
+++ estructuras_proyecto/estructura.cpp
@@ -1,2 +1,24 @@
 #include "estructura.h"
 
+struct nodo_archivo
+{
+    char *nombre;
+    Version versiones; // árbol de versiones
+};
+
+struct nodo_version
+{
+    char *numero = new char[20]; // "1", "2.1", "2.2.1", etc.
+    Texto texto;                 // Puntero a las líneas del texto
+    Version padre;               // Puntero al padre (NULL si raíz)
+    Version hijo;                // Puntero al primer hijo (subversión)
+    Version hermano;             // Puntero al siguiente hermano
+    bool esraiz;                 // Indica si es una versión raíz (primer nivel)
+};
+
+struct nodo_texto
+{
+    char *linea = new char[120];
+    unsigned int nlinea;
+    Texto sig;
+};
